8f9eadb4b5db3b02075899fe16c37e8c90649ed6
gyao1487 <gee.yao@gmail.com>
2023-10-01 18:16:15 -0500

    refactored Loading.js

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -2,7 +2,6 @@ function Loading() {
   return (
     <div className="text-center">
         <div className ="pt-100">
-      <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl"></h1>
       <div
         className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-danger motion-reduce:animate-[spin_1.5s_linear_infinite]"
         role="status"
